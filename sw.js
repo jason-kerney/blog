7850b553bfb7063e289e8200a036bdeeeff97a05
Jason Kerney <Jason@TeamKerney.com>
2024-07-31 15:21:03 -0700

    c fixing build script

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240730.1332';
+const cacheName = 'chirpy-20240731.1518';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
