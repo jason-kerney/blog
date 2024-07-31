87742c6cdd577b934d1f1a51dcd357d6d5c28448
Jason Kerney <Jason@TeamKerney.com>
2024-07-31 15:28:41 -0700

    c adding built site.

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240731.1525';
+const cacheName = 'chirpy-20240731.1528';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
