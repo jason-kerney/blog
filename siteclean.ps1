19febc1fcafe82b63d8646fab919b51a4117f4eb
Jason Kerney <Jason@TeamKerney.com>
2024-08-01 10:07:19 -0700

    c updating publish

--- siteclean.ps1
+++ siteclean.ps1
@@ -1,4 +1,3 @@
 Remove-Item .\_site\*.ps1 -Force -WarningAction Ignore
 Remove-Item .\_site\gittey-config.json -Force -WarningAction Ignore
 Copy-Item .\CNAME .\_site\
-Copy-Item .\subscribe .\subscribe
