046c3895dbf2d3de40967087bc50763e7a74c32e
Jason Kerney <Jason@TeamKerney.com>
2024-08-08 15:23:49 -0700

    e fixed header on PDD

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240808.1455';
+const cacheName = 'chirpy-20240808.1523';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
