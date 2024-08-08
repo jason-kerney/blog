91727ef4bc95e4cb8ca0cb7cd6c0a5088981a703
Jason Kerney <Jason@TeamKerney.com>
2024-08-08 14:56:50 -0700

    p pdd

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240801.1000';
+const cacheName = 'chirpy-20240808.1455';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
